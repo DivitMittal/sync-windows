941609ab5ef00bd7f5f47af104202396fcda80cc
DivitM <64.69.76.69.74.m@gmail.com>
2024-09-13 00:56:19 +0530

    Updates

--- Documents/Powershell/Profile.ps1
+++ Documents/Powershell/Profile.ps1
@@ -55,7 +55,16 @@ function Invoke-LlViaEza {
 Set-Alias -Name ll -Value Invoke-LlViaEza
 
 # yazi
-Set-Alias -Name yy -Value yazi
+function Invoke-Yazi {
+  $tmp = [System.IO.Path]::GetTempFileName()
+  yazi $args --cwd-file="$tmp"
+  $cwd = Get-Content -Path $tmp
+  if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+    Set-Location -LiteralPath $cwd
+  }
+  Remove-Item -Path $tmp
+}
+Set-Alias -Name yy -Value Invoke-Yazi
 
 # bat
 Set-Alias -Name cat -Value bat
