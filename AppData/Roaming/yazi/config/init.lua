f5ebf85dc565aaadd58fc4371a50c373cdadd3dc
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-04-13 06:14:03 +0530

    update: yazi config

--- AppData/Roaming/yazi/config/init.lua
+++ AppData/Roaming/yazi/config/init.lua
@@ -3,14 +3,14 @@ require("git"):setup()
 
 -- Add symlink target to the status bar
 function Status:name()
-	local h = cx.active.current.hovered
-	if h == nil then
-		return ui.Span("")
-	end
+  local h = cx.active.current.hovered
+  if h == nil then
+    return ui.Span("")
+  end
 
-	local linked = ""
-	if h.link_to ~= nil then
-		linked = " -> " .. tostring(h.link_to)
-	end
-	return ui.Span(" " .. h.name .. linked)
+  local linked = ""
+  if h.link_to ~= nil then
+    linked = " -> " .. tostring(h.link_to)
+  end
+  return ui.Span(" " .. h.name .. linked)
 end
