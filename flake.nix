9e8901b960fea48d945c48c431b2667a55b06268
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-05-31 02:29:02 +0530

    style: reformat via treefmt

--- flake.nix
+++ flake.nix
@@ -1,18 +1,5 @@
 {
   description = "Windows flake";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    devshell = {
-      url = "github:numtide/devshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
 
   outputs = inputs: let
     inherit (inputs.flake-parts.lib) mkFlake;
@@ -45,10 +32,22 @@
             alejandra.enable = true;
             deadnix.enable = true;
             statix.enable = true;
-            ## JSON
-            prettier.enable = true;
           };
         };
       };
     };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 }
