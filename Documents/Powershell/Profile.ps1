e71efa7eefed5e01f9eb7f0bbf9f3bb389dc4cff
Divit Mittal <64.69.76.69.74.m@gmail.com>
2026-02-18 22:11:54 +0530

    feat(pwsh): add support for microsoft store-cli

--- Documents/Powershell/Profile.ps1
+++ Documents/Powershell/Profile.ps1
@@ -15,14 +15,6 @@ $env:SHELL = "pwsh"
 #################################################################################
 # Aliases
 #################################################################################
-# windows-backup
-function Invoke-WinPackageManagerBackup {
-  $backupDir = "$env:userprofile/backup/"
-  scoop list > "$backupDir/scoop_backup.txt"
-  winget list > "$backupDir/winget_backup.txt"
-}
-Set-Alias -Name windows-backup -Value Invoke-WinPackageManagerBackup
-
 # scoop-ultimate
 function Invoke-UltimateScoop {
   scoop update
@@ -40,10 +32,17 @@ function Invoke-UltimateWinget {
 }
 Set-Alias -Name winget-ultimate -Value Invoke-UltimateWinget
 
+# store-ultimate
+function Invoke-UltimateStore {
+  store updates --apply
+}
+Set-Alias -Name store-ultimate -Value Invoke-UltimateStore
+
 # windows-ultimate
 function Invoke-UltimateWindows {
   scoop-ultimate
   winget-ultimate
+  store-ultimate
   windows-backup
 }
 Set-Alias -Name windows-ultimate -Value Invoke-UltimateWindows
@@ -51,6 +50,16 @@ Set-Alias -Name windows-ultimate -Value Invoke-UltimateWindows
 # msys
 function Invoke-Msys { msys2 -shell fish }
 Set-Alias -Name msys -Value Invoke-Msys
+
+# windows-backup
+function Invoke-WinPackageManagerBackup {
+  $backupDir = "$env:userprofile/backup/"
+  scoop list > "$backupDir/scoop_backup.txt"
+  winget list > "$backupDir/winget_backup.txt"
+  store installed > "$backupDir/store_backup.txt"
+}
+Set-Alias -Name windows-backup -Value Invoke-WinPackageManagerBackup
+
 
 #################################################################################
 # Implementation to manage dotfiles
